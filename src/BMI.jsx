95b1cb9b827f335b53b49941c54ea587c3b68f64
lazarodamasceno <lazarolawrence2@gmail.com>
2022-11-19 00:19:08 -0300

    commit

--- src/BMI.jsx
+++ src/BMI.jsx
@@ -2,8 +2,8 @@ export default function BMI(weight, height) {
     const parsedWeight = parseFloat(weight);
     const parsedHeight = parseFloat(height);
     const bmi = parsedWeight / Math.pow(parsedHeight, 2);
-    return bmi >= 18.5 && bmi <= 24.9 ? "Normal" :
-        bmi >= 25.0 && bmi <= 29.9 ? "Sobrepeso" :
-        bmi >= 30 && bmi <= 39.9 ? "Obesidade" :
-        "Obesidade grave"
+    return bmi >= 18.5 && bmi <= 24.9 ? "Normal weigth" :
+        bmi >= 25.0 && bmi <= 29.9 ? "Overweighted" :
+        bmi >= 30 && bmi <= 39.9 ? "Obesy" :
+        "Super Obesy"
 }
