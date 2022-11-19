c48e4f346f558a0ef1690d73af615304e1cff03f
Lázaro Lawrence Pereira Damasceno <62726118+LazaroDamasceno@users.noreply.github.com>
2022-11-19 00:24:34 -0300

    Update BMI.jsx

--- src/BMI.jsx
+++ src/BMI.jsx
@@ -4,6 +4,6 @@ export default function BMI(weight, height) {
     const bmi = parsedWeight / Math.pow(parsedHeight, 2);
     return bmi >= 18.5 && bmi <= 24.9 ? "Normal weigth" :
         bmi >= 25.0 && bmi <= 29.9 ? "Overweighted" :
-        bmi >= 30 && bmi <= 39.9 ? "Obesy" :
-        "Super Obesy"
+        bmi >= 30 && bmi <= 39.9 ? "Obese" :
+        "Super Obese"
 }
